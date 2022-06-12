67d2efbff84abd5b1c6706fe52ea07dac5ecfcb2
KamalDGRT <kamaldgrt@gmail.com>
2022-06-12 10:01:40 +0530

    db: create role table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 # Every model represents a table in our database.
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
 from app.database import Base
+
+
+class Role(Base):
+    __tablename__ = "role"
+
+    role_id = Column(Integer, primary_key=True, index=True)
+    role_name = Column(String(length=50), index=True, nullable=False)
+    created_at = Column(TIMESTAMP(
+        timezone=True),
+        nullable=False,
+        server_default=text('now()')
+    )
