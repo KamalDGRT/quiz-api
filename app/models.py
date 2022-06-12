43f45ad011a086dd500e7dfaca4f7979841b8bd8
KamalDGRT <kamaldgrt@gmail.com>
2022-06-12 10:04:47 +0530

    db: create role table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # Every model represents a table in our database.
 
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -12,8 +14,38 @@ class Role(Base):
 
     role_id = Column(Integer, primary_key=True, index=True)
     role_name = Column(String(length=50), index=True, nullable=False)
-    created_at = Column(TIMESTAMP(
-        timezone=True),
+    created_at = Column(
+        TIMESTAMP(timezone=True),
         nullable=False,
         server_default=text('now()')
+    )
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(length=100), nullable=False)
+    email = Column(String(length=100), nullable=False, unique=True)
+    password = Column(String(length=100), nullable=False)
+
+    role_id = Column(
+        Integer,
+        ForeignKey("role.role_id", ondelete="CASCADE"),
+        nullable=False
+    )
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text('now()')
+    )
+    updated_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=text('now()')
+    )
+
+    role = relationship(
+        "Role",
+        foreign_keys=[role_id]
     )
